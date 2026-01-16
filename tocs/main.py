68a39779209678421be137357382ee46a2d4d9df
sbundaz <bondaniniandrea@gmail.com>
2026-01-16 16:05:40 +0100

    Bump to version 1.0.7

--- tocs/main.py
+++ tocs/main.py
@@ -138,7 +138,7 @@ def main():
     parser.add_argument(
         "--depth", type=int, help="Maximum header depth to include in TOC"
     )
-    parser.add_argument("--version", action="version", version="tocs 1.0.6")
+    parser.add_argument("--version", action="version", version="tocs 1.0.7")
     parser.add_argument(
         "--dry-run",
         action="store_true",
